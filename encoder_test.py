2bf56c33143f1ac89ad9f4a6279d23e29b82f56f
thies <thies@192.168.1.101>
2017-02-13 20:01:34 +0100

    new

--- encoder_test.py
+++ encoder_test.py
@@ -4,21 +4,31 @@
 
 from pprint import pprint
 import time
+import led
 from rotary_encoder import RotaryEncoder
 
 
 # physical pin numbers
 pin_a = 22
 pin_b = 23
+colors = [0x0000FF, 0xFF0000]
+R = 17
+G = 18
+B = 27
+Multicolor = None
+
 #test
 # delay in seconds before stopping playback
 crank_delay = 3
 
 
 def main(encoder):
+    global Multicolor
     ts = time.time()
     i = 0
     last_position = 1
+    led.setup(R, G, B)
+    led.setColor(colors[0])
 
     while True:
         encoder.update()
@@ -31,18 +41,31 @@ def main(encoder):
                 # speed is seconds since last click
                 speed = time.time() - ts
                 ts = time.time()
-                i += 1
+                #i += 1
                 print 'speed:', speed, i
 
                 # check rotation direction
+                bluePart = int(i)*10
+                if bluePart <1:
+                    bluePart = 1
+                elif bluePart >255:
+                    bluePart = 254
                 if encoder.current_rotation > last_position:
                     i += 1
                     direction = 1
+                    Multicolor = int(str(bluePart),16)
                 elif encoder.current_rotation < last_position:
                     i -= 1
                     direction = 0
+                    bluePart = i*16
+                    Multicolor = int(str(bluePart),16)
+                print(bluePart)
+                print(Multicolor)
+                led.setColor(Multicolor)
+
 
                 last_position = encoder.current_rotation
+                Multicolor = int("FFFFFF",16)
 
                 # put playback code here
                 if i % 3 == 0 and direction == 1:
